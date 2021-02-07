46c0c404df935f99e7c4686edda86a4b124e8dc6
Ryan Hopkins <ryhopkins@tesla.com>
2021-02-06 19:35:42 -0500

    Fix master password storage and secret decoding

--- alcazar.py
+++ alcazar.py
@@ -3,13 +3,15 @@ import getpass
 import os
 import argparse
 import json
-from base64 import b64encode
+from base64 import b64encode, b64decode
 from cryptography.fernet import Fernet
 
 # TODO: What if user enters 'salt' or 'master_password' as a key?
 # TODO: add tests
-# TODO: Allow rehashing
-# TODO: Add backups to prevent data loss?
+# TODO: Allow rehashing and password reset
+# TODO: Add backups to prevent data loss
+# TODO: Don't ask for password unless user is retrieving or saving (no need to ask on '--help')
+# TODO: Allow more than just one secret per key
 
 def get_secrets_on_startup():
     """ Load secrets from secrets.json or create secrets.json; get Fernet session """
@@ -26,10 +28,6 @@ def get_secrets_on_startup():
 
         encrpyted_user_pass = hashlib.pbkdf2_hmac('sha256', bytes(user_password, 'utf-8'), bytes(secrets['salt'], 'utf-8'), 1000000, 32)
 
-        # TODO: Figure out why these aren't equal
-        print(secrets['master_password'])
-        print(b64encode(encrpyted_user_pass).decode('utf-8'))
-
         if secrets['master_password'] != b64encode(encrpyted_user_pass).decode('utf-8'):
             raise ValueError('Invalid password')
 
@@ -37,11 +35,11 @@ def get_secrets_on_startup():
 
     except FileNotFoundError:
         with open('secrets.json', 'w') as secrets_file:
-            salt = os.urandom(32)
+            salt = b64encode(os.urandom(32))
             encrpyted_master_pass = hashlib.pbkdf2_hmac('sha256', bytes(user_password, 'utf-8'), salt, 1000000, 32)
 
             secrets = {
-                'salt': b64encode(salt).decode('utf-8'),
+                'salt': salt.decode('utf-8'),
                 'master_password': b64encode(encrpyted_master_pass).decode('utf-8')
             }
 
@@ -58,7 +56,7 @@ def retrieve_secret(secrets, secret_name, fernet_session):
     if not secret_value:
         raise KeyError(f'{secret_name} not found')
 
-    return b64encode(fernet_session.decrypt(bytes(secret_value, 'utf-8'))).decode('utf-8')
+    return fernet_session.decrypt(b64decode(bytes(secret_value, 'utf-8'))).decode("utf-8")
 
 def save_secret(secret_name, secret_value, secrets, fernet_session):
     """ Save a secret with a given secret name and fernet session """
