d96ef0a54ea82d80f12cd838f9ff7b2f15edb0f8
Ryan Hopkins <ryhopkins@tesla.com>
2021-02-23 01:46:26 -0500

    Get new secret value in React using state

--- src/renderer/script.jsx
+++ src/renderer/script.jsx
@@ -3,11 +3,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const greetings = (
-    <h1> Hello, 世界 </h1>
-);
+// Components
+
+class Vault extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newSecret: ''
+        };
+    }
+
+    updateNewSecret(e) {
+        this.setState({
+            newSecret: e.target.value
+        });
+    }
+
+    render() {
+        return (
+            <div className="vault">
+                <input type="text" placeholder="secret" value={this.state.newSecret} onChange={e => this.updateNewSecret(e)}></input>
+                <button id="addSecretBtn" onClick={() => insertSecret(this.state.newSecret)}>Add Secret +</button>
+            </div>
+        );
+    }
+}
+
+// Render components
 
 ReactDOM.render(
-    greetings,
+    <Vault />,
     document.getElementById('root')
 );
+
+// Functions
+
+function insertSecret(secret) {
+    alert(secret);
+}
